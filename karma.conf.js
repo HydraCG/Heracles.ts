60271ad4959ac58ddb7aa4fc054674241d9b37d8
alien-mcl <alien-mcl@users.noreply.github.com>
2019-05-07 21:55:40 +0200

    More travis changes

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,49 @@
 const path = require("path");
-//const webpackConfig = require("./webpack.config");
-const webpackConfig = { module: { rules: [] } };
+const webpack = require("webpack");
+const webpackConfig = {
+  entry: "index.ts",
+  mode: "production",
+  output: {
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: [path.resolve("node_modules")]
+      },
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        include: [
+          path.resolve("src"),
+          path.resolve("integration-tests"),
+        ],
+        query: {
+          presets: [ "env", "stage-0" ]
+        },
+      },
+      {
+        test: /\.js$/,
+        loader: "source-map-loader"
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+    mainFields: ["jsnext:main", "browser", "main"]
+  },
+  devtool: "inline-source-map",
+  plugins: [
+    new webpack.SourceMapDevToolPlugin({
+      filename: null,
+      test: /\.(ts|js)($|\?)/i
+    })
+  ],
+  bail: false,
+  stats: "errors-only"
+};
 const autoWatch = process.env.npm_lifecycle_script.indexOf("--auto-watch") !== -1;
 delete webpackConfig.entry;
 webpackConfig.node = { fs: "empty" };
